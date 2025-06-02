2a88bf3c491db886aa52830fc5dc4e0a47da1f22
Gabriel Alvarez <jgabriel96@hotmail.com>
2025-06-02 16:19:48 -0600

    modified: Se agregan botones de - y +, se agrega cantidad de productos, se cambia de lugar el boton de añadir

--- src/components/tarjeta_menu/tarjeta_menu.jsx
+++ src/components/tarjeta_menu/tarjeta_menu.jsx
@@ -28,10 +28,18 @@ const TarjetaMenu = ({nombre,precio,descripcion,imagen}) => {
                 </div>
             </div>
             <div className="contenedor-botones-menu">
+                <button>
+
                 <img className='icono-resta' srcSet="../../../public/images/iconos/icono-menos.svg" alt="icono menos" onClick={() => handleDecrement(1)}/>
-                <h3 className='añadir-comida'>Añadir</h3>
+                </button>
+                <p className='cantidad-articulos'>{count}</p>
+                <button>
                 <img  className='icono-suma' srcSet="../../../public/images/iconos/icono-mas.svg" alt="icono mas" onClick={()=> handleIncrement(10)} />
+                </button>
+                <h3 className='añadir-comida'>Añadir</h3>
+                <button>
                 <img className='icono-corazon-rojo' srcSet="../../../public/images/iconos/icono-corazon-negro.svg" alt="icono corazon" />
+                </button>
             </div>
         </div>
     );
