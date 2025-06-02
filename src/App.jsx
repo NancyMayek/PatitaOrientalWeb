be57e60fbb61843650ce0fc82561bcb1780b1427
America Rodriguez <amerodriguez@outlook.com>
2025-06-01 21:28:56 -0700

    Feat:Se agrega pagina de carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
@@ -6,7 +6,7 @@ import { Inicio } from './pages/inicio/inicio'
 import { About } from './pages/about/about'
 import { Menu } from './pages/menu/menu'
 import { Menu_tematico } from './pages/menu_tematico/menu_tematico'
-import { Carrito } from './pages/carrito/carrito'
+import Carrito from './pages/carrito/carrito';
 import { Profile } from './pages/profile/profile'
 import { Registro } from './pages/registro/registro'
 import { Inicio_de_sesion } from './pages/inicio_de_sesion/inicio_de_sesion'
@@ -15,8 +15,7 @@ import { Contact } from './pages/contact/contact'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <BrowserRouter>
     <Header></Header>
