d1985396d22cac37a36400be2f841f0c5ead4c9d
Nancy Mayek <nancy.torresag@alumno.buap.mx>
2025-06-04 03:00:16 -0600

    feat: termine la pagina perfil de ususario

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,130 +1,191 @@
-import './profile.css'
+import "./profile.css";
+import { Favoritos } from "../favoritos/favoritos";
+import { Link } from "react-router-dom";
 
-const  Profile= () => {
-    return(
-        <div className="perfil-de-usuario-contenedor">
+
+const Profile = () => {
+  return (
+    <div className="perfil-de-usuario-contenedor">
+      {/* Side bar */}
       <div className="perfil-de-Usuario-SideBar">
+        <h1 className="title-profile">Perfil de usuario</h1>
         <div className="Usuario-SideBar-Boton">
           <h1>Información de usuario</h1>
         </div>
-        <div className="Usuario-SideBar-Boton">
+        <Link className="Usuario-SideBar-Boton" to="/Favoritos">
           <h1>Favoritos</h1>
-        </div>
+        </Link>
         <div className="Usuario-SideBar-Boton">
           <h1>Historial de pedidos</h1>
         </div>
       </div>
-
+      {/* Informacion de usuario */}
       <div className="informacion-de-usuario-seccion">
-        <div className="usuarurio-info-resumen">
+        <div className="usuario-info-resumen">
           <div className="usuario-foto-seccion">
             <div className="foto-container">
-            <img className="foto-de-perfil" src="../../../public/images/profile-picture/profile-picture-example.png" alt="foto de perfil" />
+              <img
+                className="foto-de-perfil"
+                src="../../../public/images/profile-picture/profile-picture-example.png"
+                alt="foto de perfil"
+              />
             </div>
-            <img className="icono-editar-foto"src="../../../public/images/iconos/icon-editar-foto.svg" alt="icono de editar foto" />
+            <img
+              className="icono-editar-foto"
+              src="../../../public/images/iconos/icon-editar-foto.svg"
+              alt="icono de editar foto"
+            />
           </div>
           <div className="usuario-informacion-principal">
-            <h1>Percy Jackson</h1>
+            <h1 className="title-profile" id="nombre-usuario ">
+              Percy Jackson
+            </h1>
             <div className="direccion">
-              <img src="../../../public/images/iconos/icon-direccion.svg" alt="icono de casa" />
+              <img
+                src="../../../public/images/iconos/icon-direccion.svg"
+                alt="icono de casa"
+              />
               <h2>Av. de la Paz #875, Col. Centro, Guadalajara</h2>
             </div>
           </div>
         </div>
+
         <form className="form-informacion-usuario">
           <div className="form-nivel-1">
-            <label htmlFor="nombre">Nombre:</label>
-            <div>
-              <input type="text" id="nombre" name="nombre" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="nombre">Nombre:</label>
+              <div className="input-grey">
+                <input type="text" id="nombre" name="nombre" value={"Percy"} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
-            <label htmlFor="apellido">Apellido:</label>
-            <div>
-              <input type="text" id="apellido" name="apellido" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="apellido">Apellido:</label>
+              <div className="input-grey">
+                <input type="text" id="apellido" name="apellido" value={"Jackson"} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
           </div>
           <div className="form-nivel-2">
-            <label htmlFor="correo">Correo:</label>
-            <div>
-              <input type="email" id="correo" name="correo" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="correo">Correo:</label>
+              <div className="input-grey">
+                <input type="email" id="correo" name="correo" value={"elLadronDelRayo@gmail.com"} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
-            <label htmlFor="telefono">Teléfono:</label>
-            <div>
-              <input type="number" id="telefono" name="telefono" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="telefono">Teléfono:</label>
+              <div className="input-grey">
+                <input type="text" id="telefono" name="telefono" value={"222-222-2334"} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
           </div>
           <div className="form-nivel-3">
-            <label htmlFor="direccion">Dirección:</label>
-            <div>
-              <input type="text" id="direccion" name="direccion" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="direccion">Dirección:</label>
+              <div className="input-grey">
+                <input type="text" id="direccion" name="direccion" value={"Av. de la Paz #875, Col. Centro, Guadalajara"} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
-            <label htmlFor="CP">Código Postal:</label>
-            <div>
-              <input type="number" id="CP" name="CP" required />
-              <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
+            <div className="elemento-de-formulario">
+              <label htmlFor="CP">Código Postal:</label>
+              <div className="input-grey">
+                <input type="number" id="CP" name="CP" value={72229} required />
+                <img
+                  src="../../../public/images/iconos/icon-editar.svg"
+                  alt="icono de editar"
+                />
+              </div>
             </div>
-          </div>
-          <label htmlFor="referencias">Referencias:</label>
-          <div>
-            <input type="text" id="referencias" name="referencias" required />
-            <img src="../../../public/images/iconos/icon-editar.svg" alt="icono de editar" />
           </div>
 
-          <h2>Métodos de pago:</h2>
-          <div className="metodos-de-pago-seccion">
+          <div className="elemento-de-formulario" id="referencias">
+            <label htmlFor="referencias">Referencias:</label>
+            <div className="input-grey">
+              <input type="text" id="referencias" name="referencias" value={"Al lado de un Oxxo"} required />
+              <img
+                src="../../../public/images/iconos/icon-editar.svg"
+                alt="icono de editar"
+              />
+            </div>
+          </div>
+
+        <div className="metodos-de-pago-seccion">
+          <h2 className="title-profile">Métodos de pago:</h2>
+            <div className="checkbox-metodos-de-pago">
             <div className="metodos-de-pago">
               <div>
-                <label htmlFor="transferencia">Pago con Transferencia</label>
                 <input
-                  type="radio"
+                  type="checkbox"
                   id="transferencia"
                   name="transferencia"
                   value="transferencia"
                   required
                 />
+                <label htmlFor="transferencia">Transferencia</label>
+                
               </div>
               <div>
-                <label htmlFor="efectivo">Pago con efectivo</label>
                 <input
-                  type="radio"
+                  type="checkbox"
                   id="efectivo"
                   name="efectivo"
                   value="efectivo"
                   required
                 />
+                <label htmlFor="efectivo">Efectivo</label>
+                
               </div>
             </div>
             <div>
               <div className="cambio">
-                <label htmlFor="cambio">Con cambio</label>
                 <input
-                  type="radio"
+                  type="checkbox"
                   id="cambio"
                   name="cambio"
                   value="cambio"
                   required
                 />
+                <label htmlFor="cambio">Con cambio</label>
+                
               </div>
               <div>
-                <label htmlFor="noCambio">Sin cambio</label>
                 <input
-                  type="radio"
+                  type="checkbox"
                   id="noCambio"
                   name="noCambio"
                   value="noCambio"
                   required
                 />
+                <label htmlFor="noCambio">Sin cambio</label>
+                
               </div>
+            </div>
             </div>
           </div>
         </form>
       </div>
     </div>
-    );
-}
+  );
+};
 
-export {Profile}
+export { Profile };
