36f841bdc7c83d9d8abfa8d3ea5f7cbeffe66fee
Francisco Caldera <a19110098@ceti.mx>
2025-06-10 23:08:06 -0600

    feat: se actualiza About Responsiva

--- src/pages/carrito/carrito.jsx
+++ src/pages/carrito/carrito.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { Context } from '../../components/context/Contex';
 import './carrito.css';
 import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
+
 
 const Carrito = () => {
 
